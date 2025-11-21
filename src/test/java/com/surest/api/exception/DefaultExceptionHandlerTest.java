6b4dfe122e7aa4ad6b363bbd31037a97eb2612f3
hemasundharp <hemapsundhar@gmail.com>
2025-11-21 16:08:48 +0530

    add tests for DefaultExceptionHandler to cover global exceptions

--- src/test/java/com/surest/api/exception/DefaultExceptionHandlerTest.java
+++ src/test/java/com/surest/api/exception/DefaultExceptionHandlerTest.java
@@ -26,7 +26,6 @@ class DefaultExceptionHandlerTest {
     void handleAuthenticationException_returnsUnauthorized() {
         Exception ex = new RuntimeException("Auth failed");
         ResponseEntity<RestError> response = exceptionHandler.handleAuthenticationException(ex);
-
         assertEquals(401, response.getStatusCodeValue());
         assertEquals("Authentication failed at controller advice", response.getBody().getMessage());
         assertEquals("401 UNAUTHORIZED", response.getBody().getDetails());
@@ -37,7 +36,6 @@ class DefaultExceptionHandlerTest {
     void handleInvalidLoginException_returnsUnauthorized() {
         InvalidLoginException ex = new InvalidLoginException("Invalid credentials");
         ResponseEntity<RestError> response = exceptionHandler.handleInvalidLoginException(ex);
-
         assertEquals(401, response.getStatusCodeValue());
         assertEquals("Invalid credentials", response.getBody().getMessage());
         assertEquals("401 UNAUTHORIZED", response.getBody().getDetails());
@@ -48,7 +46,6 @@ class DefaultExceptionHandlerTest {
     void handleAccessDeniedException_returnsForbidden() {
         AccessDeniedException ex = new AccessDeniedException("Access denied");
         ResponseEntity<RestError> response = exceptionHandler.handleAccessDeniedException(ex);
-
         assertEquals(403, response.getStatusCodeValue());
         assertEquals("Access denied", response.getBody().getMessage());
         assertEquals("403 FORBIDDEN", response.getBody().getDetails());
@@ -59,7 +56,6 @@ class DefaultExceptionHandlerTest {
     void handleUserNotFoundException_returnsNotFound() {
         UserNotFoundException ex = new UserNotFoundException("User not found");
         ResponseEntity<RestError> response = exceptionHandler.handle(ex);
-
         assertEquals(404, response.getStatusCodeValue());
         assertEquals("User not found", response.getBody().getMessage());
         assertEquals("404 NOT_FOUND", response.getBody().getDetails());
@@ -70,7 +66,6 @@ class DefaultExceptionHandlerTest {
     void myAnyExceptionHandler_returnsBadRequest() {
         Exception ex = new Exception("Some error");
         ResponseEntity<RestError> response = exceptionHandler.myAnyExceptionHandler(ex, webRequest);
-
         assertEquals(400, response.getStatusCodeValue());
         assertEquals("Some error", response.getBody().getMessage());
         assertEquals("Mocked request description", response.getBody().getDetails());
