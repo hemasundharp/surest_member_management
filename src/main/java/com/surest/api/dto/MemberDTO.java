97003dbd63cc466d538aef0098c4ffd6508bee76
hemasundharp <hemapsundhar@gmail.com>
2025-11-21 15:47:57 +0530

    update member DTO

--- src/main/java/com/surest/api/dto/MemberDTO.java
+++ src/main/java/com/surest/api/dto/MemberDTO.java
@@ -1,5 +1,10 @@
 package com.surest.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,9 +12,21 @@ import java.util.UUID;
 
 @Data
 public class MemberDTO {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private UUID id;
+
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @NotNull(message = "Date of birth is required")
+    @Past(message = "Date of birth must be in the past")
     private LocalDate dateOfBirth;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
 }
