6fa69a284ab18731b767a167e6e1fc8a39ae9c23
hemasundharp <hemapsundhar@gmail.com>
2025-11-21 15:50:19 +0530

    update role DTO

--- src/main/java/com/surest/api/dto/RoleDTO.java
+++ src/main/java/com/surest/api/dto/RoleDTO.java
@@ -1,9 +1,11 @@
 package com.surest.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RoleDTO {
+    @NotBlank(message = "Role name is required")
+    private String name;
 
-	private String name;
 }
