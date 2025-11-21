d8bf0307678394c2b2f1f09691269f62cc14edf1
hemasundharp <hemapsundhar@gmail.com>
2025-11-21 16:00:13 +0530

    update MemberService interface with CRUD and pagination methods

--- src/main/java/com/surest/api/service/MemberService.java
+++ src/main/java/com/surest/api/service/MemberService.java
@@ -1,19 +1,19 @@
 package com.surest.api.service;
 
-import java.util.UUID;
 import com.surest.api.dto.MemberDTO;
 import com.surest.api.dto.MemberPaginatedResponse;
-import com.surest.api.model.Member;
+
+import java.util.UUID;
 
 public interface MemberService {
 
-    Member createMember(MemberDTO dto);
+    MemberDTO createMember(MemberDTO dto);
 
     MemberPaginatedResponse getAllMembers(int page, int size, String sortBy, String sortDir);
 
-    Member getMemberById(UUID id);
+    MemberDTO getMemberById(UUID id);
 
-    Member updateMemberById(UUID id, MemberDTO dto);
+    MemberDTO updateMemberById(UUID id, MemberDTO dto);
 
     void deleteMemberById(UUID id);
 }
