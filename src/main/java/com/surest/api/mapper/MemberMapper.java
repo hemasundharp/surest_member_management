0ca2403c24faeef96f05e579770e6317e28a2cf4
hemasundharp <hemapsundhar@gmail.com>
2025-11-21 15:50:52 +0530

    update member mapper

--- src/main/java/com/surest/api/mapper/MemberMapper.java
+++ src/main/java/com/surest/api/mapper/MemberMapper.java
@@ -4,18 +4,16 @@ import com.surest.api.dto.MemberDTO;
 import com.surest.api.model.Member;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
 
-    MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
-
     // Map Entity to DTO
-   // @Mapping(source = "dateOfBirth", target = "dob")
     MemberDTO toDto(Member member);
 
     // Map DTO to Entity
-   // @Mapping(source = "dob", target = "dateOfBirth")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Member toEntity(MemberDTO dto);
 }
