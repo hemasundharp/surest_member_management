f224f412c811c373d69b28926992b96bdd71232a
hemasundharp <hemapsundhar@gmail.com>
2025-11-21 15:56:00 +0530

    refactor Role model with updated fields and validation

--- src/main/java/com/surest/api/model/Role.java
+++ src/main/java/com/surest/api/model/Role.java
@@ -1,25 +1,32 @@
 package com.surest.api.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
 @Entity
 @Data
 @Table(name = "role")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
 
     @Column(nullable = false, unique = true, length = 50)
     private String name;
+
+    @Version
+    private Long version;
 }
 
