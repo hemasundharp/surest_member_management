d1164628ac50220b4dc97b7b9a26081cea82aa32
hemasundharp <hemapsundhar@gmail.com>
2025-11-21 15:51:16 +0530

    update role mapper

--- src/main/java/com/surest/api/mapper/RoleMapper.java
+++ src/main/java/com/surest/api/mapper/RoleMapper.java
@@ -3,13 +3,15 @@ package com.surest.api.mapper;
 import com.surest.api.dto.RoleDTO;
 import com.surest.api.model.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
 
-    RoleMapper INSTANCE = Mappers.getMapper(RoleMapper.class);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
     // DTO to Entity
     Role toEntity(RoleDTO dto);
 
